b6956892398858bedfcb0d9ad6054232106f33c7
juan.hoyos <juan.hoyo@grupo1522.com>
2025-08-14 19:37:54 -0500

    Fix: eliminando importación innecesaria.

--- src/main/java/com/his/security/controller/AuthController.java
+++ src/main/java/com/his/security/controller/AuthController.java
@@ -8,7 +8,6 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.apache.tomcat.util.bcel.Const;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
